0349d8fc848763ada989093f8adc82f00ef8acd4
Vlad Shmidov <shmidovish@gmail.com>
2021-02-15 11:20:26 +0300

    Lesson_1

--- Lesson_1/src/com/company/GuessTheNumber.java
+++ Lesson_1/src/com/company/GuessTheNumber.java
@@ -1,7 +1,19 @@
 package com.company;
 
 public class GuessTheNumber {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        int guessNumber = 69;
+        int personNumber = 64;
+        while (personNumber != guessNumber) {
+            if (personNumber > guessNumber) {
+                System.out.println("Ваше число" + personNumber + "больше того, что загадал компьютер");
+                personNumber--;
+            }
+            if (personNumber < guessNumber) {
+                System.out.println("Ваше число" + personNumber + "меньше того, что загадал компьютер");
+                personNumber++;
+            }
+        }
+        System.out.println("Вы угадали");
     }
 }
